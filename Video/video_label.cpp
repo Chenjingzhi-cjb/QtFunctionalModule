b271b05324a07ff22d70506d5cf53be23e797904
Chenjingzhi-cjb <1286576927@qq.com>
2024-01-22 23:23:32 +0800

    Updated "video label" (untested code)
    This update mainly provides two classes about "Video Label", including:
    1. The "VideoLabel" class is used when image data can be obtained from memory directly using the API interface. This class can be used after custom implementation of the getImage() and processImage() methods. You can also customize a derived class of this class and override the getImage() and processImage() methods.
    2. The "VideoLabelTriggered" class is used when the API interface can only be used to trigger the update of the image file, and then the image file data must be re-read. This class can also be used directly or inherited.

--- Video/video_label.cpp
+++ Video/video_label.cpp
@@ -1,17 +1,61 @@
 #include "video_label.hpp"
 
+#include <random>
 
-void VideoLabelThread::run() {
-    // 延时
-    QThread::msleep(m_interval_time);
 
-    // 更新图像
-    m_video_label->updateImage();
-
-    // 刷新控件
-    m_video_label->update();
+VideoLabelThread::VideoLabelThread(VideoLabel *video_label, unsigned int sleep_time, QObject *parent)
+        : QThread(parent),
+          m_video_label(video_label),
+          m_interval_time(sleep_time),
+          m_get_finish_flag(false),
+          m_thread_end_flag(false) {
+    connect(this, &VideoLabelThread::signalGetImage, m_video_label, &VideoLabel::getImageDecorator, Qt::QueuedConnection);
+    connect(this, &VideoLabelThread::signalPaintImage, m_video_label, &VideoLabel::paintImage, Qt::QueuedConnection);
+    connect(m_video_label, &VideoLabel::signalGetImageFinish, this, [=](){ m_get_finish_flag.store(true); });
 }
 
-void VideoLabel::updateImage() {
+VideoLabelThread::~VideoLabelThread() {
+    while (!m_thread_end_flag.load()) {
+        m_get_finish_flag.store(true);
+        m_thread_end_flag.store(true);
+    }
 
+    quit();
+    wait();
+}
+
+void VideoLabelThread::run() {
+    while (!m_thread_end_flag.load()) {
+        // 延时
+        QThread::msleep(m_interval_time);
+
+        // 获取图像
+        emit signalGetImage();
+        while (!m_get_finish_flag.load()) {
+            QThread::msleep(1);
+        }
+        m_get_finish_flag.store(false);
+
+        // 处理图像
+        m_video_label->processImage();
+
+        // 刷新控件
+        emit signalPaintImage();
+    }
+}
+
+void VideoLabel::getImage() {
+    // 调用 api 获取更新图像
+    // m_image = externalGetImageApi();
+}
+
+void VideoLabel::processImage() {
+}
+
+void VideoLabelTriggered::getImage() {
+    // 调用 api 触发更新图像文件，再读取图像文件
+    // externalUpdateImageApi();
+}
+
+void VideoLabelTriggered::processImage() {
 }
--- Video/video_label.hpp
+++ Video/video_label.hpp
@@ -3,6 +3,10 @@
 
 #include <QLabel>
 #include <QThread>
+#include <QTimer>
+
+#include <Windows.h>
+#include <atomic>
 
 #include "opencv2/opencv.hpp"
 
@@ -11,13 +15,12 @@ class VideoLabel;
 
 
 class VideoLabelThread : public QThread {
-public:
-    VideoLabelThread(VideoLabel *video_label, unsigned int sleep_time = 40, QObject *parent = nullptr)
-        : QThread(parent),
-          m_video_label(video_label),
-        m_interval_time(sleep_time) {}
+    Q_OBJECT;
 
-    ~VideoLabelThread() = default;
+public:
+    VideoLabelThread(VideoLabel *video_label, unsigned int sleep_time = 1000, QObject *parent = nullptr);
+
+    ~VideoLabelThread();
 
 public:
     void setIntervalTime(unsigned int interval_time) {
@@ -27,10 +30,18 @@ public:
 protected:
     void run() override;
 
+signals:
+    void signalGetImage();
+
+    void signalPaintImage();
+
 private:
     VideoLabel *m_video_label;
 
     unsigned int m_interval_time;  // ms
+
+    std::atomic_bool m_get_finish_flag;
+    std::atomic_bool m_thread_end_flag;
 };
 
 
@@ -39,8 +50,11 @@ class VideoLabel : public QLabel {
 
 public:
     explicit VideoLabel(QWidget *parent = nullptr)
-        : QLabel(parent),
-          m_thread(new VideoLabelThread(this)) {}
+            : QLabel(parent),
+              m_thread(new VideoLabelThread(this)) {
+        connect(m_thread, &QThread::finished, m_thread, &QObject::deleteLater);
+        m_thread->start();
+    }
 
     ~VideoLabel() {
         delete m_thread;
@@ -51,13 +65,42 @@ public:
         m_thread->setIntervalTime(interval_time);
     }
 
-    // 更新图像，对图像进行相关处理
-    virtual void updateImage();
+    bool readImage(const QString &image_path, int flags = cv::IMREAD_COLOR) {
+        FILE* f = nullptr;
+        errno_t err = _wfopen_s(&f, string2wstring(image_path.toStdString()).c_str(), L"rb");
 
-protected:
-    void paintEvent(QPaintEvent *event) override {
-        Q_UNUSED(event);
+        if (err != 0) {
+            std::cout << "readImage() error: Image file failed to open, error code is " << err << std::endl;
+            return false;
+        }
 
+        fseek(f, 0, SEEK_END);
+        size_t buffer_size = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        std::vector<char> buffer(buffer_size);
+        fread(&buffer[0], sizeof(char), buffer_size, f);
+
+        fclose(f);
+
+        cv::Mat image = cv::imdecode(buffer, flags);
+
+        image.copyTo(m_image);
+
+        return true;
+    }
+
+    // 获取 图像数据 到 m_image，主线程执行
+    virtual void getImage();
+
+    virtual void getImageDecorator() {
+        getImage();
+        emit signalGetImageFinish();
+    }
+
+    // 对 获取到的 m_image 进行进一步处理，子线程执行
+    virtual void processImage();
+
+    void paintImage() {
         if (!m_image.empty()) {
             // 将图像缩放到控件的尺寸
             cv::Mat scaled_image;
@@ -65,28 +108,76 @@ protected:
 
             // 将 OpenCV 的 Mat 转换为 Qt 的 QImage
             QImage qimage;
-            if (m_image.channels() == 3) {
+            if (scaled_image.channels() == 3) {
                 qimage = QImage(scaled_image.data, (int) scaled_image.cols, (int) scaled_image.rows,
                                 (int) scaled_image.step, QImage::Format_BGR888);
-            } else if (m_image.channels() == 1) {
+            } else if (scaled_image.channels() == 1) {
                 qimage = QImage(scaled_image.data, (int) scaled_image.cols, (int) scaled_image.rows,
                                 (int) scaled_image.step, QImage::Format_Grayscale8);
             } else {  // Error
                 return;
             }
 
-            // 将 QImage 转化为 QPixmap
-            QPixmap qpixmap = QPixmap::fromImage(qimage);
-
-            // 显示 QPixmap
-            this->setPixmap(qpixmap);
+            this->setPixmap(QPixmap::fromImage(qimage));
         }
     }
 
-private:
+protected:
+    std::wstring string2wstring(const std::string &str) {
+        int len = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
+        std::wstring wstr(len, L'\0');
+        MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, const_cast<wchar_t *>(wstr.data()), len);
+        wstr.resize(wcslen(wstr.c_str()));
+        return wstr;
+    }
+
+signals:
+    void signalGetImageFinish();
+
+protected:
     VideoLabelThread *m_thread;
 
     cv::Mat m_image;
+};
+
+
+class VideoLabelTriggered : public VideoLabel {
+public:
+    void setImageInfo(std::string image_path, int image_flags = cv::IMREAD_COLOR) {
+        m_image_path = image_path;
+        m_image_flags = image_flags;
+    }
+
+    virtual void getImage() override;
+
+    void getImageDecorator() override {
+        getImage();
+        loadTheImage();
+    }
+
+    virtual void processImage() override;
+
+protected:
+    bool readTheImage() {
+        return readImage(QString::fromStdString(m_image_path), m_image_flags);
+    }
+
+    void loadTheImage() {
+        QTimer::singleShot(m_load_image_time, this, [=]() {
+            bool status = readTheImage();
+            if (!status) {
+                loadTheImage();
+            } else {
+                emit signalGetImageFinish();
+            }
+        });
+    }
+
+protected:
+    std::string m_image_path;
+    int m_image_flags;
+
+    const int m_load_image_time = 5;
 };
 
 
