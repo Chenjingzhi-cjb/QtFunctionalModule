5c688ac876530b97fbcbf8eaa56181add0766cea
Chenjingzhi-cjb <1286576927@qq.com>
2024-07-12 16:30:57 +0800

    Update json_config.hpp

--- Config/json_config.hpp
+++ Config/json_config.hpp
@@ -16,32 +16,11 @@
 using json_t = nlohmann::json;
 
 
-class JsonConfigParam {
+class JsonConfigReader {
 public:
-    std::string exec_path;
-
-    int aa;
-    std::string bb;
-    std::vector<int> cc;
-    std::vector<std::string> dd;
-    std::vector<json_t> ee;
-};
-
-
-class JsonConfigReader : public JsonConfigParam {
-private:
-    JsonConfigReader() {
-        initConfig();
-    }
-
-    ~JsonConfigReader() = default;
+    virtual ~JsonConfigReader() = default;
 
 public:
-    static JsonConfigReader &get_instance() {
-        static JsonConfigReader instance;
-        return instance;
-    }
-
     void loadConfig(const QString &config_path) {
         QFile config_file(config_path);
         if (config_file.open(QIODevice::ReadOnly)) {
@@ -51,11 +30,8 @@ public:
 
             try {
                 json_t config_json_obj = json_t::parse(config_info);
-                aa = config_json_obj.at("aa").get<int>();
-                bb = config_json_obj.at("bb").get<std::string>();
-                bb = config_json_obj.at("cc").get<std::vector<int>>();
-                bb = config_json_obj.at("dd").get<std::vector<std::string>>();
-                cc = config_json_obj.at("ee").get<std::vector<json_t>>();
+                
+                parseConfig(config_json_obj);
             } catch (json_t::exception &e) {
                 std::cout << "Failed to parse the config file: " << e.what() << std::endl;
             }
@@ -64,27 +40,20 @@ public:
         }
     }
 
-private:
-    void initConfig() {
-        exec_path = QCoreApplication::applicationDirPath().toStdString();
-    }
+protected:
+    virtual void parseConfig(json_t &config_json_obj) = 0;
 };
 
 
-class JsonConfigWriter : public JsonConfigParam {
+class JsonConfigWriter {
 public:
-    JsonConfigWriter() = default;
-
-    ~JsonConfigWriter() = default;
+    virtual ~JsonConfigWriter() = default;
 
 public:
     void saveConfig(const QString &config_path) {
         json_t config_json_obj;
-        config_json_obj["aa"] = aa;
-        config_json_obj["bb"] = bb;
-        config_json_obj["cc"] = cc;
-        config_json_obj["dd"] = dd;
-        config_json_obj["ee"] = ee;
+        
+        buildConfig(config_json_obj);
 
         QFile config_file(config_path);
         if (config_file.open(QIODevice::WriteOnly)) {
@@ -99,6 +68,69 @@ public:
         } else {
             std::cout << "Failed to open the config file for writing." << std::endl;
         }
+    }
+
+protected:
+    virtual void buildConfig(json_t &config_json_obj) = 0;
+};
+
+
+class JsonConfigParam {
+public:
+    int aa;
+    std::string bb;
+    std::vector<int> cc;
+    std::vector<std::string> dd;
+    std::vector<json_t> ee;
+};
+
+
+class JsonConfigReaderExample : public JsonConfigReader, public JsonConfigParam {
+private:
+    JsonConfigReaderExample() {
+        initConfig();
+    }
+
+    ~JsonConfigReaderExample() override = default;
+
+public:
+    static JsonConfigReaderExample &get_instance() {
+        static JsonConfigReaderExample instance;
+        return instance;
+    }
+
+protected:
+    void parseConfig(const json_t &config_json_obj) override {
+        aa = config_json_obj.at("aa").get<int>();
+        bb = config_json_obj.at("bb").get<std::string>();
+        bb = config_json_obj.at("cc").get<std::vector<int>>();
+        bb = config_json_obj.at("dd").get<std::vector<std::string>>();
+        cc = config_json_obj.at("ee").get<std::vector<json_t>>();
+    }
+
+private:
+    void initConfig() {
+        exec_path = QCoreApplication::applicationDirPath().toStdString();
+    }
+
+public:
+    std::string exec_path;
+};
+
+
+class JsonConfigWriterExample : public JsonConfigWriter, public JsonConfigParam {
+public:
+    JsonConfigWriterExample() = default;
+
+    ~JsonConfigWriterExample() override = default;
+
+protected:
+    void buildConfig(json_t &config_json_obj) override {
+        config_json_obj["aa"] = aa;
+        config_json_obj["bb"] = bb;
+        config_json_obj["cc"] = cc;
+        config_json_obj["dd"] = dd;
+        config_json_obj["ee"] = ee;
     }
 };
 
